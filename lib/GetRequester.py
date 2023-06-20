6cc941572a246960a56273978aa468a8c741a39a
manoti <manotimike@gmail.com>
2023-06-20 15:49:27 +0300

    Done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -3,11 +3,17 @@ import json
 
 class GetRequester:
 
-    def __init__(self, url):
+     def __init__(self, url):
         self.url = url
 
-    def get_response_body(self):
-        pass
+     def get_response_body(self):
+        response = requests.get(self.url)
+        return response.content
+    
+     def load_json(self):
+        employees = []
 
-    def load_json(self):
-        pass
+        employees_list = json.loads(self.get_response_body())
+        for employee in employees_list:
+            employees.append(employee)
+        return employees
